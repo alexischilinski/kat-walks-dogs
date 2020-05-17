67a57728c6453cf74bd47c449fe843d5366a09cc
alexiscait142 <chilinski.a@gmail.com>
2020-05-17 12:23:39 -0400

    remove container from app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 import Header from './components/Header'
 import {BrowserRouter as Router, Switch, Route, useHistory} from 'react-router-dom'
 import {Home} from './components/Home'
-import {Container} from './components/Container'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <Container/> */}
       <Route exact path="/"><Home/></Route>
     </div>
   );
