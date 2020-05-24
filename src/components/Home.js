ef11100c950e39589432535f8d1b161837a582a8
alexiscait142 <chilinski.a@gmail.com>
2020-05-24 12:14:52 -0400

    edit about kat

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export const Home = () => {
         <div className="home">
             <div className="profile">
                 <img className="photo" src="https://images.unsplash.com/photo-1502673530728-f79b4cab31b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80"></img>
-                <div className="blurb">My name is Kat DiMascio. I am a graduate of Becker College with an Associate Degree in Animal Care and Management. Kat Walks Dogs is a unique dog walking service dog training and adventure in our walks. Lets get go dog out there and moving!</div>
+                <div className="blurb">My name is Kat DiMascio. I am a graduate of Becker College with an Associate Degree in Animal Care and Management. Kat Walks Dogs is a unique experience for your dog in that I can assist with training or just provide simple daily exercise! Lets get your dog out there and moving!</div>
             </div>
         </div>
     )
