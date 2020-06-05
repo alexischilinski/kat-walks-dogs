9e209ccbbfa8119989272f2ce866903464130954
alexiscait142 <chilinski.a@gmail.com>
2020-06-05 13:50:51 -0400

    add scrolling feature to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import Contact from './Contact'
+import { Link, animateScroll as scroll } from 'react-scroll'
 
 class Navbar extends Component{
+
+    scrollTo = () => {
+        scroll.scrollToBottom()
+    }
+
     render(){
         return(
             <div className="navbar">
-                <h2>About</h2>
+                <button className="navbutton">About</button>
                 {/* <h2>Photos</h2> */}
                 {/* <h2>Reviews</h2> */}
-                <h2>Contact</h2>
+                <button className="navbutton" onClick={this.scrollTo}>Contact</button>
             </div>
         )
     }
