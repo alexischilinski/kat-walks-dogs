7747f527db0ed259cdabaa5a75875636033f89c2
alexiscait142 <chilinski.a@gmail.com>
2020-06-08 13:43:10 -0400

    format homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,13 @@ import './Home.css'
 export const Home = () => {
     return (
         <div className="home">
+            <h1>I'm Kat DiMascio!</h1>
             <div className="profile">
                 {/* <img className="photo" src="https://images.unsplash.com/photo-1502673530728-f79b4cab31b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80"></img> */}
                 <img className="photo" src={katPhoto}></img>
                 <div className="blurb-div">
-                    <div className="blurb intro">My name is Kat DiMascio. I am a graduate of Becker College with an Associate Degree in Animal Care and Management.</div>
-                    <div className="blurb info">Kat Walks Dogs is a unique experience for your dog in that I can assist with training or just provide simple daily exercise!</div>
+                    <div className="blurb intro">I am a graduate of Becker College with an Associate Degree in Animal Care and Management.</div>
+                    <div className="blurb info">My goal is to give your dog a unique outdoor experience that can be breed-specific. I can assist with training or just provide simple daily exercise!</div>
                     <div className="blurb end">Let's get your dog out there and moving!</div>
                 </div>
             </div>
--- src/components/Rates.js
+++ src/components/Rates.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './Rates.css'
 
 export const Rates = () => {
     return (
