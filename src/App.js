a42d3b986fbead402d616507c64a130d00cf34cc
alexiscait142 <chilinski.a@gmail.com>
2020-06-10 14:31:42 -0400

    add rate information

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import Header from './components/Header'
 import {BrowserRouter as Router, Switch, Route, useHistory} from 'react-router-dom'
 import {Home} from './components/Home'
--- src/components/Rates.js
+++ src/components/Rates.js
@@ -5,6 +5,18 @@ export const Rates = () => {
     return (
         <div>
             <h1 className="rate-title">Rates</h1>
+            <div className="rate-div">
+                <p className="text">
+                    I aim to provide a breed-specific experience for your dog, complete with fun outdoor activities
+                    and optional training.
+                </p>
+                <p className="text">
+                    My general rate is $30 an hour.
+                </p>
+                <p className="text">
+                    Please contact me for scheduling information and further pricing details.
+                </p>
+            </div>
         </div>
     )
 }
