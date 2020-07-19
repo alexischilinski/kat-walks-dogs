171d68a42b532e6684469c28452f09743ad123ff
alexiscait142 <chilinski.a@gmail.com>
2020-07-19 17:26:42 -0400

    fix forms styling for adding reviews

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -2,10 +2,28 @@ import './Reviews.css'
 import React, { Component } from 'react'
 
 class Review extends Component {
+
+    state = {
+        name: "",
+        review: ""
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render(){
         return(
-            <div>
-                <h1 className="rate-title">Reviews</h1>
+            <div className="review-div">
+                <h1 className="review-title">Reviews</h1>
+                <h3 className="text">Leave a review:</h3>
+                <form className="review-form">
+                    <input type="text" name="name" placeholder="Name (e.g. John D.)" onChange={this.handleChange}></input>
+                    <textarea name="review" placeholder="Write your review here" onChange={this.handleChange}></textarea>
+                    <button class="submit-button" type="submit">Submit</button>
+                </form>
             </div>
         )
     }
